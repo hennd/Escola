be1f854a2e432a8a521627127e727fce7abf712a
hennd <rafael.andrade@ulbra.inf.br>
2020-06-14 21:15:49 -0300

    ultima de domingo a noite funcionando o login para a tela menu

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -5,30 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.EventListener;
 
 public class Menu extends AppCompatActivity {
+
     Button botaoCadastrarAluno = findViewById(R.id.btnCadastrarAluno);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
